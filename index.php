f502436d9c535b61a9d1d448aefe10d1061753e3
milik qiyaa <tasqiyaa28@gmail.com>
2025-11-21 09:43:06 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Belajar Git</title>
+</head>
+<body>
+    <h1>Halaman Percobaan Git</h1>
+
+    <p>
+        <?php 
+            echo "Ini halaman sederhana untuk latihan Git. Silakan ubah isi teks ini, lalu lakukan commit dan push.";
+        ?>
+    </p>
+
+    <ul>
+        <li>Intan Tasqiyatul Mudmainnah</li>
+        <li>23.N1.0027</li>
+        <li>Manajemen project</li>
+    </ul>
+
+    <p>
+        <?php 
+            echo "Waktu sekarang: " . date("d-m-Y H:i:s");
+        ?>
+    </p>
+</body>
+</html>
